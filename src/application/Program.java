f33bc651610b6642e9e3d5939a86d308eb94706e
Gustavo <gustavonoia51@gmail.com>
2024-05-06 15:00:23 -0300

    Commit 2, creating classes.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow World!");
+		
+		Board board = new Board(8, 8);
+		
+		System.out.println(board);
 
 	}
 
