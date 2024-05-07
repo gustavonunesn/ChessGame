57d6dc4515e47f4bd033afef2ce90baad1aa771c
Gustavo <gustavonoia51@gmail.com>
2024-05-07 10:58:42 -0300

    Commit #6, add improves/positions/pieces/modify background

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,11 +22,17 @@ public class ChessMatch {
 		 }
 		 return mat;
 	}
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
 	
 	public ChessPiece perfomChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -39,6 +45,15 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position.");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no movement possibles to this piece.");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The piece can't be moved to this target");
 		}
 	}
 	
