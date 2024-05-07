1b1d6c5b7907756d54ecd242930f4976fa851776
Gustavo <gustavonoia51@gmail.com>
2024-05-07 09:40:52 -0300

    #5, add improves and exceptions

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import chess.ChessException;
+
 public class Board {
 	
 	private Integer rows;
@@ -39,6 +41,18 @@ public class Board {
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new ChessException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
